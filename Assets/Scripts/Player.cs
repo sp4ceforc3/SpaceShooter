14822c3af5ffa8cba860083948d5ada21b553ec0
sp4ceforc3 <85077306+sp4ceforc3@users.noreply.github.com>
2023-05-25 19:48:57 +0200

    New Assets, PLyer Explosion, Explosion and Destruction on Projectile hit
    -- import assets for explosion
    -- added particle system prefab for explosion
    -- added player Explosion and destruction on projectile hit
    -- added enemy basic projectile prefab

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,7 +7,10 @@ public class Player : MonoBehaviour
 {
 
     [SerializeField] float speed = 1f;
+    [SerializeField] ParticleSystem explosion;
+    [SerializeField] GameObject player;
     Rigidbody2D rb;
+    bool destroyed = false;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +19,10 @@ public class Player : MonoBehaviour
     }
 
     private void FixedUpdate() {
-       rb.velocity =  Input.GetAxisRaw("Vertical") * Vector3.up * speed * Time.fixedDeltaTime;
+        if(!destroyed)
+        {
+            rb.velocity =  Input.GetAxisRaw("Vertical") * Vector3.up * speed * Time.fixedDeltaTime;
+        }        
     }
 
     // Update is called once per frame
@@ -25,14 +31,31 @@ public class Player : MonoBehaviour
 
     }
     
+    // TODO: Expand this function with music fading and return to Main Menu (because delay needed)
+    IEnumerator PlayerLooseEffects()
+    {
+        ParticleSystem tmp = Instantiate(explosion);
+        tmp.transform.position = player.transform.position;
+        tmp.Play();
+
+        yield return new WaitForSeconds(1.5f);
+
+        Destroy(player);
+    }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("This is a log message.");
         switch (collision.gameObject.tag)
         {
             case "Border":
-                Debug.Log("This is a log message.");
                 rb.velocity = new Vector3(0, 0, 0);
+                // TODO: This is just a Test dummy to see, whether explosion works
+                Instantiate(explosion, this.transform).Play();
+                break;
+            case "EnemyProjectile":
+                destroyed = true;
+                StartCoroutine("PlayerLooseEffects");
+                // TODO: Save the Highcore here or in the case statement
                 break;
             default:
                 break;
