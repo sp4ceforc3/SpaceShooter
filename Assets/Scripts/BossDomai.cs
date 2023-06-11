0a8ef32726b9aeac1d1e9c4753699f31befd9314
Domai <github@domai-tb.com>
2023-06-11 12:50:30 +0200

    - fix bugging behavoiur of DomaiBoss - add ability to not rotate at player for enemies by random chance

--- Assets/Scripts/BossDomai.cs
+++ Assets/Scripts/BossDomai.cs
@@ -11,31 +11,37 @@ public class BossDomai : EnemyHandling
 
     private float lastShot = 0f;
 
-    public override void Movement()
-    {       
-        // Rotation
-        Vector2 playerPos = (Vector2) player.transform.position;
-        Vector2 lookDirection = ((Vector2) gameObject.transform.position - playerPos).normalized;
-        gameObject.transform.up = lookDirection;
+    private void Start() {
+        hp = data.hp;
+        speed = data.movespeed;
+    }
 
-        // Movement
-        if(rb != null)
-            rb.velocity = -direction * gameObject.transform.up * speed * 0.25f*Time.fixedDeltaTime;
+    private void FixedUpdate() {
+        if(!destroyed) {
+            // Rotation
+            Vector2 playerPos = (Vector2) player.transform.position;
+            Vector2 lookDirection = ((Vector2) gameObject.transform.position - playerPos).normalized;
+            gameObject.transform.up = lookDirection;
 
-        // Shooting
-        if ((lastShot - Time.time) <= -1.5f) {
-            lastShot = Time.time;
+            // Movement
+            if (rb != null)
+                rb.velocity = -direction * gameObject.transform.up * speed * 0.25f*Time.fixedDeltaTime;
 
-            Instantiate(projectile, firePointR.transform.position, firePointR.transform.rotation * Quaternion.Euler(0, 0, 15));
-            Instantiate(projectile, firePointL.transform.position, firePointL.transform.rotation * Quaternion.Euler(0, 0, -15));
-            Instantiate(projectileC, firePointC.transform.position, firePointC.transform.rotation);
+            // Shooting
+            if ((lastShot - Time.time) <= -1.5f) {
+                lastShot = Time.time;
 
-            if (Random.value <= 0.33f) {
-                Vector3 fireDirection = firePointC.transform.up.normalized;
-                Instantiate(projectileC, firePointC.transform.position + 2.2f*fireDirection, firePointC.transform.rotation);
-                Instantiate(projectileC, firePointC.transform.position + 1.2f*fireDirection, firePointC.transform.rotation * Quaternion.Euler(0, 0, 10));
-                Instantiate(projectileC, firePointC.transform.position, firePointC.transform.rotation * Quaternion.Euler(0, 0, -10));
+                Instantiate(projectile, firePointR.transform.position, firePointR.transform.rotation * Quaternion.Euler(0, 0, 15));
+                Instantiate(projectile, firePointL.transform.position, firePointL.transform.rotation * Quaternion.Euler(0, 0, -15));
+                Instantiate(projectileC, firePointC.transform.position, firePointC.transform.rotation);
+
+                if (Random.value <= 0.33f) {
+                    Vector3 fireDirection = firePointC.transform.up.normalized;
+                    Instantiate(projectileC, firePointC.transform.position + 2.2f*fireDirection, firePointC.transform.rotation);
+                    Instantiate(projectileC, firePointC.transform.position + 1.2f*fireDirection, firePointC.transform.rotation * Quaternion.Euler(0, 0, 10));
+                    Instantiate(projectileC, firePointC.transform.position, firePointC.transform.rotation * Quaternion.Euler(0, 0, -10));
+                }
             }
-        }
+        }        
     }
 }
